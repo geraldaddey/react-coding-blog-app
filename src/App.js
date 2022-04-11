653997ab159e5a4df261302a0569cad8bf9fd540
geraldaddey <geraldaddey@yahoo.com>
2022-04-11 12:45:33 +0000

    new component styles added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import Home from "./home";
 
 // Add styles to your component
 import "./App.css";
+import "./home.css";
 
 function App() {
   // We  can declare any variable here and use it in the JSX
 
-  const title = "Mr Gerald Addey";
+  const title = "Gerald Addey";
   const likes = 50;
   const link = "http://www.google.com";
 
   return (
     <div className="App">
       <div className="content">
-        <h1>Blog App created by {title} </h1>
+        <p>Food Blog</p>
 
-        <Home />
-        <Navbar />
-        <p>{10}</p>
-        <p>{"hello, guys"}</p>
-        <p>The picture had {likes} likes in total.</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}> Google Home Page</a>
-        <p className="footer">Gerald Addey</p>
+        <div className="links">
+          <a href="/home">Home</a>
+          <a href="/create">New Blog</a>
+        </div>
+
+        <a href="/create"></a>
       </div>
+
+      <div className="footer">{title}</div>
     </div>
   );
 }
