25d6ead4b637b7a865b9eee679135f78d5ba4890
geraldaddey <geraldaddey@yahoo.com>
2022-04-21 14:33:30 +0000

    passing functions as props

--- src/Blog.js
+++ src/Blog.js
@@ -8,26 +8,34 @@ const Blog = () => {
       title: "My new website",
       body: "Lorem ipsum dolor sit amet",
       author: "Gerald",
+      id: 1,
     },
     {
       title: "My new book",
       body: "Lorem ipsum dolor sit amet",
-      author: "Kweku",
+      author: "Gerald",
+      id: 2,
     },
     {
       title: "My new web3 roadmap",
       body: "Lorem ipsum dolor sit amet",
       author: "Quansah",
+      id: 3,
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="blog-home">
-      <BlogList blogs={blogs} />;
-      <BlogList
+      <BlogList blogs={blogs} handleDelete={handleDelete} />;
+      {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "Gerald")}
         title="Gerald's Blogs"
-      />
+      /> */}
     </div>
   );
 };
