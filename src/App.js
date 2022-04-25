03c16df378bcfb7f526f5b90437bc047176d4771
geraldaddey <geraldaddey@yahoo.com>
2022-04-25 12:53:22 +0000

    route added

--- src/App.js
+++ src/App.js
@@ -3,41 +3,33 @@ import "./App.css";
 import "./home.css";
 import Home from "./home";
 import Blog from "./Blog";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./navbar";
 
+function App() {
+  // const link = "http://www.google.com";
 
-  const link = "http://www.google.com";
+  // const handleClick = () => {
+  //   console.log(title);
+  // };
 
-  const handleClick = () => {
-    console.log(title);
-  };
-
-  const handleClickAgain = (name) => {
-    console.log("new name is " + title + " " + name);
-  };
+  // const handleClickAgain = (name) => {
+  //   console.log("new name is " + title + " " + name);
+  // };
 
   return (
-    <div className="App">
-      {/* <Home /> */}
-      <Blog />
-
-      {/* <div className="content">
-        <p>Food Blog</p>
-
-        <div className="links">
-          <a href="/home">Home</a>
-          <a href="/create">New Blog</a>
-          <a href={link}>About Us</a>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Blog />
+            </Route>
+          </Switch>
         </div>
-
-        <button onClick={handleClick}>Click Me</button>
-        <button onClick={() => handleClickAgain("mario")}>
-          Click ME AGAIN
-        </button>
-
-        <div className="footer"> {title} </div>
-      </div> */}
-    </div>
+      </div>
+    </Router>
   );
 }
 
