9d063e68f2069d2037f0cde2c82fc9d27c62302d
geraldaddey <geraldaddey@yahoo.com>
2022-04-25 11:24:45 +0000

    new commit

--- src/Blog.js
+++ src/Blog.js
@@ -5,20 +5,29 @@ import "./home.css";
 const Blog = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        // console.log(res);
+        if (!res.ok) {
+          throw Error("could not fetch that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsLoading(false);
-      });
+        setError(null);
+      })
+      .catch((err) => setError(err.message));
+    setIsLoading(false);
   }, []);
 
   return (
     <div className="blog-preview">
+      {error && <div> {error} </div>}
       {isLoading && <div> Loading ....</div>}
       {blogs && <BlogList blogs={blogs} />}
     </div>
